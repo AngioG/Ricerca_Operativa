47b81a16d4e316704670c3b7c7b3204bb71b1144
AngioG <72987164+AngioG@users.noreply.github.com>
2022-10-12 21:19:36 +0200

    Add files via upload

--- Angioletti_Logistica/frm_main.Designer.cs
+++ Angioletti_Logistica/frm_main.Designer.cs
@@ -30,6 +30,13 @@
         {
             this.lbl_title = new System.Windows.Forms.Label();
             this.pan_controls = new System.Windows.Forms.Panel();
+            this.chk_random = new System.Windows.Forms.CheckBox();
+            this.nud_tot = new System.Windows.Forms.NumericUpDown();
+            this.label5 = new System.Windows.Forms.Label();
+            this.nud_max = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.nud_min = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
             this.btn_gen = new System.Windows.Forms.Button();
             this.btn_chnge_table = new System.Windows.Forms.Button();
             this.nud_cons = new System.Windows.Forms.NumericUpDown();
@@ -38,6 +45,9 @@
             this.label1 = new System.Windows.Forms.Label();
             this.dgv_main = new System.Windows.Forms.DataGridView();
             this.pan_controls.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nud_tot)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud_max)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud_min)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.nud_cons)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.nud_prod)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dgv_main)).BeginInit();
@@ -57,6 +67,13 @@
             // 
             // pan_controls
             // 
+            this.pan_controls.Controls.Add(this.chk_random);
+            this.pan_controls.Controls.Add(this.nud_tot);
+            this.pan_controls.Controls.Add(this.label5);
+            this.pan_controls.Controls.Add(this.nud_max);
+            this.pan_controls.Controls.Add(this.label4);
+            this.pan_controls.Controls.Add(this.nud_min);
+            this.pan_controls.Controls.Add(this.label3);
             this.pan_controls.Controls.Add(this.btn_gen);
             this.pan_controls.Controls.Add(this.btn_chnge_table);
             this.pan_controls.Controls.Add(this.nud_cons);
@@ -66,24 +83,142 @@
             this.pan_controls.Dock = System.Windows.Forms.DockStyle.Top;
             this.pan_controls.Location = new System.Drawing.Point(0, 50);
             this.pan_controls.Name = "pan_controls";
-            this.pan_controls.Size = new System.Drawing.Size(1400, 100);
+            this.pan_controls.Size = new System.Drawing.Size(1400, 105);
             this.pan_controls.TabIndex = 1;
+            // 
+            // chk_random
+            // 
+            this.chk_random.AutoSize = true;
+            this.chk_random.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.chk_random.Location = new System.Drawing.Point(265, 48);
+            this.chk_random.Name = "chk_random";
+            this.chk_random.Size = new System.Drawing.Size(83, 25);
+            this.chk_random.TabIndex = 13;
+            this.chk_random.Text = "Casuale";
+            this.chk_random.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            this.chk_random.UseVisualStyleBackColor = true;
+            this.chk_random.CheckedChanged += new System.EventHandler(this.chk_random_CheckedChanged);
+            // 
+            // nud_tot
+            // 
+            this.nud_tot.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.nud_tot.Increment = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.nud_tot.Location = new System.Drawing.Point(475, 20);
+            this.nud_tot.Maximum = new decimal(new int[] {
+            1500,
+            0,
+            0,
+            0});
+            this.nud_tot.Minimum = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            this.nud_tot.Name = "nud_tot";
+            this.nud_tot.Size = new System.Drawing.Size(50, 25);
+            this.nud_tot.TabIndex = 12;
+            this.nud_tot.Value = new decimal(new int[] {
+            750,
+            0,
+            0,
+            0});
+            this.nud_tot.ValueChanged += new System.EventHandler(this.nud_tot_ValueChanged);
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(265, 20);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(205, 25);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Totale merce da trasportare:";
+            // 
+            // nud_max
+            // 
+            this.nud_max.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.nud_max.Location = new System.Drawing.Point(860, 64);
+            this.nud_max.Maximum = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            this.nud_max.Minimum = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            this.nud_max.Name = "nud_max";
+            this.nud_max.Size = new System.Drawing.Size(50, 25);
+            this.nud_max.TabIndex = 10;
+            this.nud_max.Value = new decimal(new int[] {
+            30,
+            0,
+            0,
+            0});
+            this.nud_max.ValueChanged += new System.EventHandler(this.Conflitto_nud);
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(675, 64);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(190, 25);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Costo massimo trasporti:";
+            // 
+            // nud_min
+            // 
+            this.nud_min.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.nud_min.Location = new System.Drawing.Point(860, 20);
+            this.nud_min.Maximum = new decimal(new int[] {
+            30,
+            0,
+            0,
+            0});
+            this.nud_min.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nud_min.Name = "nud_min";
+            this.nud_min.Size = new System.Drawing.Size(50, 25);
+            this.nud_min.TabIndex = 8;
+            this.nud_min.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.nud_min.ValueChanged += new System.EventHandler(this.Conflitto_nud);
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(675, 20);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(190, 25);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Costo minimo trasporti:";
             // 
             // btn_gen
             // 
             this.btn_gen.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.btn_gen.Location = new System.Drawing.Point(375, 15);
+            this.btn_gen.Location = new System.Drawing.Point(936, 15);
             this.btn_gen.Name = "btn_gen";
             this.btn_gen.Size = new System.Drawing.Size(75, 75);
             this.btn_gen.TabIndex = 6;
             this.btn_gen.TabStop = false;
-            this.btn_gen.Text = "Genera dati casuali";
+            this.btn_gen.Text = "Genera nuovi dati";
             this.btn_gen.UseVisualStyleBackColor = true;
+            this.btn_gen.Click += new System.EventHandler(this.btn_gen_Click);
             // 
             // btn_chnge_table
             // 
             this.btn_chnge_table.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.btn_chnge_table.Location = new System.Drawing.Point(275, 15);
+            this.btn_chnge_table.Location = new System.Drawing.Point(548, 14);
             this.btn_chnge_table.Name = "btn_chnge_table";
             this.btn_chnge_table.Size = new System.Drawing.Size(75, 75);
             this.btn_chnge_table.TabIndex = 5;
@@ -95,14 +230,14 @@
             // nud_cons
             // 
             this.nud_cons.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.nud_cons.Location = new System.Drawing.Point(185, 60);
+            this.nud_cons.Location = new System.Drawing.Point(185, 65);
             this.nud_cons.Maximum = new decimal(new int[] {
-            40,
+            20,
             0,
             0,
             0});
             this.nud_cons.Minimum = new decimal(new int[] {
-            1,
+            2,
             0,
             0,
             0});
@@ -114,18 +249,19 @@
             0,
             0,
             0});
+            this.nud_cons.ValueChanged += new System.EventHandler(this.change_tot_max);
             // 
             // nud_prod
             // 
             this.nud_prod.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.nud_prod.Location = new System.Drawing.Point(185, 15);
+            this.nud_prod.Location = new System.Drawing.Point(185, 20);
             this.nud_prod.Maximum = new decimal(new int[] {
-            40,
+            20,
             0,
             0,
             0});
             this.nud_prod.Minimum = new decimal(new int[] {
-            1,
+            2,
             0,
             0,
             0});
@@ -137,11 +273,12 @@
             0,
             0,
             0});
+            this.nud_prod.ValueChanged += new System.EventHandler(this.change_tot_max);
             // 
             // label2
             // 
             this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label2.Location = new System.Drawing.Point(15, 60);
+            this.label2.Location = new System.Drawing.Point(15, 65);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(175, 25);
             this.label2.TabIndex = 1;
@@ -150,7 +287,7 @@
             // label1
             // 
             this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(15, 15);
+            this.label1.Location = new System.Drawing.Point(15, 20);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(175, 25);
             this.label1.TabIndex = 0;
@@ -164,7 +301,9 @@
             this.dgv_main.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgv_main.Location = new System.Drawing.Point(25, 175);
             this.dgv_main.Name = "dgv_main";
+            this.dgv_main.RowHeadersWidth = 50;
             this.dgv_main.RowTemplate.Height = 25;
+            this.dgv_main.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
             this.dgv_main.Size = new System.Drawing.Size(1350, 600);
             this.dgv_main.TabIndex = 2;
             // 
@@ -180,6 +319,10 @@
             this.Text = "Logistica - Angioletti";
             this.Load += new System.EventHandler(this.frm_main_Load);
             this.pan_controls.ResumeLayout(false);
+            this.pan_controls.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nud_tot)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud_max)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud_min)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.nud_cons)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.nud_prod)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dgv_main)).EndInit();
@@ -198,5 +341,12 @@
         private Label label2;
         private Label label1;
         private DataGridView dgv_main;
+        private NumericUpDown nud_max;
+        private Label label4;
+        private NumericUpDown nud_min;
+        private Label label3;
+        private CheckBox chk_random;
+        private NumericUpDown nud_tot;
+        private Label label5;
     }
 }
--- Angioletti_Logistica/frm_main.cs
+++ Angioletti_Logistica/frm_main.cs
@@ -1,3 +1,6 @@
+using System.Data;
+using System.Windows.Forms;
+
 namespace Angioletti_Logistica
 {
     public partial class frm_main : Form
@@ -9,31 +12,220 @@ namespace Angioletti_Logistica
 
         private void frm_main_Load(object sender, EventArgs e)
         {
+
         }
 
         private void btn_chnge_table_Click(object sender, EventArgs e)
         {
-            int x = 0;
-            int y = 0;
-            while (x < nud_cons.Value)
+            #region setup
+            dgv_main.Columns.Clear();
+            DataGridViewCell dataGridViewCell = new DataGridViewTextBoxCell();
+            dgv_main.AutoGenerateColumns = false;
+            #endregion
+            #region columns
+
+            for (int i = 0; i < nud_cons.Value; i++)
             {
-                DataGridViewColumn col = new DataGridViewColumn();
-
-                col.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
-                col.Name = $"col{x+1}";
-                col.HeaderText = $"D{x+1}";
-
-                dgv_main.Columns.Add(col);
-
-                x++;
+                DataGridViewColumn column = new DataGridViewColumn();
+                column.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+                column.HeaderText = $"P{i+1}";
+                column.CellTemplate = dataGridViewCell;
+                dgv_main.Columns.Add(column);
             }
 
-            while(y < nud_prod.Value)
-            {
-                dgv_main.Rows.Add();
+            DataGridViewColumn last_column = new DataGridViewColumn();
+            last_column.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            last_column.HeaderText = "Necessari";
+            last_column.CellTemplate = dataGridViewCell;
+            last_column.ReadOnly = true;
+            dgv_main.Columns.Add(last_column);
+            #endregion
+            #region rows
+            dgv_main.RowHeadersWidth = dgv_main.Columns[0].Width;
 
-                y++;
+            for (int i = 0; i < nud_prod.Value; i++)
+            {
+                DataGridViewRow row = (DataGridViewRow) dgv_main.RowTemplate.Clone();
+                row.CreateCells(dgv_main);
+
+                row.HeaderCell.Value = $"Up{i + 1}";
+
+                for (int x = 0; x < nud_cons.Value; x++)
+                {
+                    row.Cells[x].Value = 1;
+                }
+
+                dgv_main.Rows.Add(row);
             }
+
+            DataGridViewRow last_row = (DataGridViewRow)dgv_main.RowTemplate.Clone();
+            last_row.CreateCells(dgv_main);
+            last_row.HeaderCell.Value = $"Disponibili";
+            last_row.ReadOnly = true;
+            dgv_main.Rows.Add(last_row);
+            #endregion
+            #region genera dati
+            if (dgv_main.Columns.Count == 0)
+            {
+                MessageBox.Show("Devi prima creare una tabella", "Errore", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            Random rng = new Random();
+
+            for (int r = 0; r < dgv_main.Rows.Count - 1; r++)
+                for (int c = 0; c < dgv_main.Columns.Count - 1; c++)
+                {
+                    int n = rng.Next((int)nud_min.Value, (int)nud_max.Value);
+                    dgv_main.Rows[r].Cells[c].Value = n;
+                }
+
+            if (chk_random.Checked)
+            {
+                int min = (int)(dgv_main.Columns.Count > dgv_main.Rows.Count ? dgv_main.Columns.Count - 1 : dgv_main.Rows.Count - 1);
+                min = (2 * min + 1) * 10;
+                DatiCasuali(new Random().Next(min, 150));
+            }
+            else
+            {
+                DatiCasuali((int)nud_tot.Value / 10);
+            }
+            #endregion
+        }
+
+        public void DatiCasuali(int Tot)
+        {
+            dgv_main.Rows[dgv_main.Rows.Count - 1].Cells[dgv_main.Columns.Count - 1].Value = Tot*10;
+            Random rng = new Random();
+
+            #region consumatori
+            int[] dati_u = new int[dgv_main.Columns.Count-1];
+            int Tot_u = Tot + dgv_main.Columns.Count - 2;
+
+            
+            for (int i = 0; i < dati_u.Count(); i++)
+            {
+                int a = 0;
+                do
+                {
+                    a = rng.Next(1, Tot_u);
+                } while (dati_u.Contains(a) || dati_u.Contains(a+1) || dati_u.Contains(a-1));
+
+                dati_u[i] = a;
+            }
+
+
+            dati_u = dati_u.OrderBy(n => n).ToArray();
+
+            for (int i = 0; i < dgv_main.Columns.Count - 2; i++)
+                dgv_main.Rows[dgv_main.Rows.Count - 1].Cells[i].Value = (dati_u[i] - 1 - (i == 0 ? 0 : dati_u[i-1]))*10;
+            dgv_main.Rows[dgv_main.Rows.Count - 1].Cells[dgv_main.Columns.Count - 2].Value = (Tot_u - dati_u[dati_u.Count() - 2])*10;
+            #endregion
+            #region produttori
+            int[] dati_p = new int[dgv_main.Rows.Count - 1];
+            int Tot_p = Tot + dgv_main.Rows.Count - 2;
+
+
+            for (int i = 0; i < dati_p.Count(); i++)
+            {
+                int a = 0;
+                do
+                {
+                    a = rng.Next(1, Tot_p);
+                } while (dati_p.Contains(a) || dati_p.Contains(a+1) || dati_p.Contains(a-1));
+
+                dati_p[i] = a;
+            }
+
+
+            dati_p = dati_p.OrderBy(n => n).ToArray();
+
+            for (int i = 0; i < dgv_main.Rows.Count - 2; i++)
+                dgv_main.Rows[i].Cells[dgv_main.Columns.Count - 1].Value = (dati_p[i] - 1 - (i == 0 ? 0 : dati_p[i - 1]))*10;
+            dgv_main.Rows[dgv_main.Rows.Count - 2].Cells[dgv_main.Columns.Count - 1].Value = (Tot_p - dati_p[dati_p.Count() - 2])*10;
+            #endregion
+        }
+
+        private void Conflitto_nud(object sender, EventArgs e)
+        {
+            if (sender == null || sender.GetType() != typeof(NumericUpDown))
+                return;
+
+            NumericUpDown nud = sender as NumericUpDown;
+
+            if(nud.Name == "nud_min")
+            {
+                if(nud.Value >= nud_max.Value)
+                    nud_max.Value = nud.Value + 1;
+            }
+
+            if (nud.Name == "nud_max")
+            {
+                if (nud.Value <= nud_min.Value)
+                    nud_min.Value = nud.Value - 1;
+            }
+        }
+
+        private void btn_gen_Click(object sender, EventArgs e)
+        {
+            if(dgv_main.Columns.Count == 0)
+            {
+                MessageBox.Show("Devi prima creare una tabella", "Errore", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            Random rng = new Random();
+
+            for (int r = 0; r < dgv_main.Rows.Count-1; r++)
+                for (int c = 0; c < dgv_main.Columns.Count-1; c++)        
+                {
+                    int n = rng.Next((int)nud_min.Value, (int)nud_max.Value);
+                    dgv_main.Rows[r].Cells[c].Value = n;
+                }
+
+            if (chk_random.Checked)
+            {
+                int min = (int)(dgv_main.Columns.Count > dgv_main.Rows.Count ? dgv_main.Columns.Count - 1 : dgv_main.Rows.Count - 1);
+                min = (2 * min + 1) * 10;
+                DatiCasuali(new Random().Next(min, 150));
+            }
+            else
+            {
+                DatiCasuali((int)nud_tot.Value / 10);
+            }
+        }
+
+        private void chk_random_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chk_random.Checked)
+                nud_tot.Enabled = false;
+            else
+                nud_tot.Enabled = true;
+        }
+
+        private void change_tot_max(object sender, EventArgs e)
+        {
+            int max = (int)(nud_prod.Value > nud_cons.Value ? nud_prod.Value : nud_cons.Value);
+
+            max = (2 * max + 1) * 10;
+
+            if (nud_tot.Value < max)
+                nud_tot.Value = max;
+        }
+
+        private void nud_tot_ValueChanged(object sender, EventArgs e)
+        {
+            /*nud_prod.Maximum = Math.Floor(nud_tot.Value / 20);
+            nud_cons.Maximum = Math.Floor(nud_tot.Value / 20);*/
+            var val = nud_tot.Value;
+
+            if (nud_prod.Value >= Math.Floor(val / 20))
+                nud_prod.Value = Math.Floor(val / 20);
+
+            if (nud_cons.Value >= Math.Floor(val / 20))
+                nud_cons.Value = Math.Floor(val / 20);
+
+            nud_tot.Value = val;
         }
     }
 }
